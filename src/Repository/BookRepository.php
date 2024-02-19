fb565392a1323c783f9d25197a7d6e50fd05c7e7
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-02-19 16:32:29 +0100

    Templates

---
+++ src/Repository/BookRepository.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Book;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use function Doctrine\ORM\QueryBuilder;
+
+/**
+ * @extends ServiceEntityRepository<Book>
+ *
+ * @method Book|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Book|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Book[]    findAll()
+ * @method Book[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class BookRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Book::class);
+    }
+
+    public function findBooksBetween(?\DateTimeImmutable $start = null, ?\DateTimeImmutable $end = null): array
+    {
+        $qb = $this->createQueryBuilder('b')
+            ->select('b');
+
+        if ($start) {
+            $qb->andWhere($qb->expr()->gte('b.releasedAt', ':start'))
+                ->setParameter('start', $start);
+        }
+
+        if ($end) {
+            $qb->andWhere($qb->expr()->lte('b.releasedAt', ':end'))
+                ->setParameter('end', $end);
+        }
+
+        return $qb->getQuery()
+            ->getResult();
+    }
+
+//    /**
+//     * @return Book[] Returns an array of Book objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('b')
+//            ->andWhere('b.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('b.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Book
+//    {
+//        return $this->createQueryBuilder('b')
+//            ->andWhere('b.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
